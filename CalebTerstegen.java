db83e0cfff3f0a07d976259e2a6a3a5a95a670c5
CalebTerstegen <calebterstegen@vt.edu>
2026-09-03 18:11:30 -0400

    First Commit

---
+++ CalebTerstegen.java
@@ -0,0 +1,12 @@
+public class CalebTerstegen
+{
+    // ~ Fields ................................................................
+
+    // ~ Constructors ..........................................................
+
+    // ~Public Methods ........................................................
+    public static void main(String[] args)
+    {
+        System.out.println("Your Name is on the team.");
+    }
+}
