c34dcc06630218a2468db7a28a8629585218633c
CalebTerstegen <calebterstegen@vt.edu>
2026-09-22 19:15:37 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -81,9 +81,88 @@ public class UserInterface
     }
     
     /**
-     * Allows for accessing an account for purposes such as depositing/withdrawing
+     * Allows for deposit for an account
      */
-    public void accessAccount() {
-        
+    public void deposit() {
+        input = "";
+        CheckingAccount account;
+        float deposit = 0;
+        System.out.println("You are choosing to deposit");
+        System.out.println("");
+        while(true) {
+            System.out.println("Please type the account number of the account you would like to deposit into");
+            input = scanner.nextLine();
+            if(input.toLowerCase().equals("exit")) {
+                return;
+            }
+            try {
+            account = accounts.getAccount(Integer.parseInt(input));
+            }
+            catch(Exception e) {
+                System.out.println("Invalid account number");
+                continue;
+            }
+            break;
+        }
+        while(true) {
+            System.out.println("Depositing into account " + input + " please input amount to deposit");
+            input = scanner.nextLine();
+            if(input.toLowerCase().equals("exit")) {
+                return;
+            }
+            try {
+            deposit = Float.parseFloat(input);
+            }
+            catch(Exception e) {
+                System.out.println("Invalid deposit amount");
+                continue;
+            } 
+            System.out.println("Depositing $" + deposit);
+            account.deposit(deposit);
+            return;
+        }
+    }
+    
+    /**
+     * Allows for withdraw for an account
+     */
+    public void withdraw() {
+        input = "";
+        CheckingAccount account;
+        float withdraw = 0;
+        System.out.println("You are choosing to withdraw");
+        System.out.println("");
+        while(true) {
+            System.out.println("Please type the account number of the account you would like to withdraw from");
+            input = scanner.nextLine();
+            if(input.toLowerCase().equals("exit")) {
+                return;
+            }
+            try {
+            account = accounts.getAccount(Integer.parseInt(input));
+            }
+            catch(Exception e) {
+                System.out.println("Invalid account number");
+                continue;
+            }
+            break;
+        }
+        while(true) {
+            System.out.println("Withdrawing from account " + input + " please input amount to withdraw");
+            input = scanner.nextLine();
+            if(input.toLowerCase().equals("exit")) {
+                return;
+            }
+            try {
+            withdraw = Float.parseFloat(input);
+            }
+            catch(Exception e) {
+                System.out.println("Invalid withdraw amount");
+                continue;
+            } 
+            System.out.println("Depositing $" + withdraw);
+            account.withdraw(withdraw);
+            return;
+        }
     }
 }
