8bb3f39ebf97cbd4fa3e60b53c388323305a28d3
CalebTerstegen <calebterstegen@vt.edu>
2026-09-24 12:12:03 -0400

    Delete old commit test files

--- AaronRay.java
+++
@@ -1,5 +0,0 @@
-public class AaronRay {
-    public static void main(String[] args) {
-        System.out.println("Aaron Ray is on the team.");
-    }
-}
--- AryanPatil.java
+++
@@ -1,5 +0,0 @@
-public class Aryan {
-    public static void main(String[] args) {
-        System.out.println("Aryan is on the team.");
-    }
-}
